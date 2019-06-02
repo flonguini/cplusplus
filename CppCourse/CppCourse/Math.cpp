52415dcc5a6994ca799ace2482672c91799f98a1
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-06-02 01:28:13 -0300

    #include #define and #if

---
+++ CppCourse/CppCourse/Math.cpp
@@ -0,0 +1,12 @@
+// Define a constant, where I type PI the compiler will convert into 3.1415
+#define PI 3.1415
+
+// include the souce code from iostream file
+#include <iostream>
+
+#if 1
+int multiply(int a, int b) {
+	int result = a * b;
+	return result;
+}
+#endif
