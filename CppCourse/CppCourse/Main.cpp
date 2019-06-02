6dab330c7745d0ec86a18b26d15edd72c332ddd4
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-06-02 14:19:32 -0300

    Conditions and branches

--- CppCourse/CppCourse/Main.cpp
+++ CppCourse/CppCourse/Main.cpp
@@ -1,9 +1,24 @@
+// <> for compiler files
 #include <iostream>
+// "" for every thing else, it's relative path "../Log.h" is up one directory
 #include "Log.h"
 
 int main() 
 {
-	InitLog("Initializing Log");
-	Log("Hello World");
+	int x = 5;
+	if (x == 5) 
+	{
+		InitLog("Initializing Log");
+		Log("Hello World");
+	}
+	else if (x == 6) 
+	{
+		Log("X is 6");
+	}
+	else
+	{
+		Log("Other value");
+	}
+
 	std::cin.get();
 }
