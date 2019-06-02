5b8429600ece2b476af2bb7a7be32e91c90d95ba
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-06-02 15:08:59 -0300

    Control Flow continue, break and return

--- CppCourse/ProjectStructure/src/Main.cpp
+++ CppCourse/ProjectStructure/src/Main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+
+	for (int i = 1; i < 20; i++)
+	{
+		//if (i % 2 == 0)
+		//	continue; // go to next iteration
+
+		if (i % 5 == 0)
+			break; // stop the iteration
+		
+		std::cout << "Hello World ";
+		std::cout << i << std::endl;
+
+	}
 
 }
