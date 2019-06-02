9f68224f022abe4d6d8c4cb4be8d888a3a4ce77a
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-06-02 00:55:23 -0300

    Function to print a string as parameter

--- CppCourse/CppCourse/Main.cpp
+++ CppCourse/CppCourse/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+// const char* type that can hold strings
+void Log(const char* message) {
+	std::cout << message << std::endl;
+}
+
 int main() {
-	std::cout << "Hello World from C++" << std::endl;
+	Log("Hello World from C++");
 	std::cin.get();
 }
